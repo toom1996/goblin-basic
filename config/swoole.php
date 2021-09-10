13afd393a4f4176b85c02dcdf60bef19b2698a2f
1023150697@qq.com <1023150697@qq.com>
2021-09-10 18:54:58 +0800

    update

--- config/swoole.php
+++ config/swoole.php
@@ -1,11 +1,15 @@
 <?php
+
 return [
     'host' => "0.0.0.0",
     'port' => 9502,
+    'event' => [
+        \toom1996\event\SwooleEvent::SWOOLE_ON_START => [\toom1996\http\StartCallback::class, 'onStart'],
+    ],
     'setting' => [
-        'enable_static_handler' => APP_DEBUG,
-        'document_root' => APP_PATH . '/web',
-        'worker_num' => 1,
+//        'enable_static_handler' => APP_DEBUG,
+//        'document_root' => APP_PATH . '/web',
+        'worker_num' => 2,
         'enable_coroutine' => true,
         'hook_flags' => SWOOLE_HOOK_ALL
     ]
